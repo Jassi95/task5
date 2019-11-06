0d069098cc4bb2fa3398ef3204d12ac87a27a07e
Juhnu95 <you@example.com>
2019-11-06 18:54:04 +0200

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var Promise = require("bluebird");
-
+//.
 // Define routers
 var indexRouter = require('./routes/index');
 
